88e0faff389f0d4b1cefadfa22101a1461f6af5f
autumn-ragland <raglanda998@gmail.com>
2020-02-18 17:00:49 -0600

    graded

--- index.js
+++ index.js
@@ -3,57 +3,45 @@
 // ### Exercise 1:
 // Created  name id and address and checked to see if it showed in the console
 
-let my_name =("Kevin");
-let my_id = ("2112");
-let my_address = ("Memphis,TN");
+// let my_name = ("Kevin");
+// let my_id = ("2112");
+// let my_address = ("Memphis,TN");
 
-let my_template_string = 
-        `My name is ${my_name}
-        My id is ${my_id} 
-        My address is ${my_address}`
-console.log(my_template_string) 
-
-
-
+// let my_template_string =
+//   `My name is ${my_name}
+//         My id is ${my_id} 
+//         My address is ${my_address}`
+// console.log(my_template_string)
 
 // ### Exercise 2:
-
 // Created a template string 
 
-let contestant = prompt("1_M_l337!");
-let score =prompt("9001");
-console.log(`Congratulations + 1_M_l337! !! == your score is 9001`);
-
-
-
-
-
+// let contestant = prompt("1_M_l337!");
+// let score = prompt("9001");
+// !! : You are not passing in a variable and you have unncessary plus signs and equal signs
+// console.log(`Congratulations + 1_M_l337! !! == your score is 9001`);
 
 
 // ### Exercise 3:
 
 // reated home team and visiting team and prompted user to enter hometeam and visiting team and enter score
 
-let userHometeam =prompt(`Home Team Name`);
-if ("Grizzlies")
-  {
-   alert(`102`)
-}
+// let userHometeam = prompt(`Home Team Name`);
+// if ("Grizzlies") {
+//   alert(`102`)
+// }
 
-let userVisitingteam =prompt(`Visiting Team Name`);
-if ("GS Warriors")
-{
-    alert(`23`)
-    
-}
-if (`hometeamscore`) 
-{
-    (`==102`)    
-} else 
-{
-  (``)  
-} 
+// let userVisitingteam = prompt(`Visiting Team Name`);
+// if ("GS Warriors") {
+//   alert(`23`)
 
-let userScore1 = prompt(`Grizzlies Team score`);
-let userScore2 = prompt(`GS Warriors Team score`)
+// }
+// if (`hometeamscore`) {
+//   (`==102`)
+// } else {
+//   (``)
+// }
+
+// let userScore1 = prompt(`Grizzlies Team score`);
+// let userScore2 = prompt(`GS Warriors Team score`)
 
